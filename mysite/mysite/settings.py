eb612c7a71236ab71a347137ad6dd980aa04849c
Katsiaryna Laban <katsiaryna.laban@ventionteams.com>
2024-06-17 10:23:05 +0300

    Создали миграции, настроили маршрут для хранения поля photo при DEBAG=True, в settings прописали путь

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_URL
+
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
