16a419403208f891313cd097d2a13ca766a1a277
Katsiaryna Laban <katsiaryna.laban@ventionteams.com>
2024-06-17 14:02:50 +0300

    Added class Meta

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -11,4 +11,10 @@ class News(models.Model):
     def __str__(self):
         return self.title
     
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
+        
+    
 
