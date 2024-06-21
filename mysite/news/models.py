bf1616efc2952c013839a3e859cb95570661ccf3
Katsiaryna Laban <katsiaryna.laban@ventionteams.com>
2024-06-21 16:51:57 +0300

    Added add-news with add_news.html and forms.py

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,13 +2,37 @@ from django.db import models
 from django.urls import reverse
 
 class News(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Наименование')
-    content = models.TextField(blank=True, verbose_name='Контент')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикация')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    title = models.CharField(
+        max_length=150, 
+        verbose_name='Наименование'
+    )
+    content = models.TextField(
+        blank=True, 
+        verbose_name='Контент'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, 
+        verbose_name='Дата публикация'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, 
+        verbose_name='Обновлено'
+    )
+    photo = models.ImageField(
+        upload_to='photos/%Y/%m/%d/', 
+        verbose_name='Фото',
+        blank=True
+    )
+    is_published = models.BooleanField(
+        default=True, 
+        verbose_name='Опубликовано'
+    )
+    category = models.ForeignKey(
+        'Category', 
+        on_delete=models.PROTECT, 
+        null=True, 
+        verbose_name='Категория'
+    )
     
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"news_id": self.pk})    
@@ -27,7 +51,6 @@ class Category(models.Model):
     
     def get_absolute_url(self):
         return reverse('category', kwargs={"category_id": self.pk})
-    
     
     def __str__(self):
         return self.title
