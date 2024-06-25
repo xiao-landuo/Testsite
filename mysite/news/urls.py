0ba866750a20287952647bd7ee68a164360cd07d
Katsiaryna Laban <katsiaryna.laban@ventionteams.com>
2024-06-25 12:45:27 +0300

    Классы дл работы с views, forms - ModelForm вместо Form

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -20,8 +20,12 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name ='home'),
-    path('category/<int:category_id>/', get_category, name = 'category'),
-    path('news/<int:news_id>/', view_news, name = 'view_news'),
-    path('news/add-news/', add_news, name = 'add_news'),
+    # path('', index, name ='home'),
+    path('', HomeNews.as_view(), name ='home'),
+    # path('category/<int:category_id>/', get_category, name = 'category'),
+    path('category/<int:category_id>/', NewsByCategory.as_view(extra_context={'title':'Какой-то тайтл'}), name = 'category'),
+    # path('news/<int:news_id>/', view_news, name = 'view_news'),
+    path('news/<int:pk>/', ViewNews.as_view(), name = 'view_news'),
+    # path('news/add-news/', add_news, name = 'add_news'),
+    path('news/add-news/', CreateNews.as_view(), name = 'add_news'),
 ]
